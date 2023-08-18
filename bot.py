cf0e2caf4ffe7af15ff7d50ebba22dc067dd0712
redbaron2k7 <76609332+redbaron2k7@users.noreply.github.com>
2023-08-17 22:43:30 -0500

    Oh I'm stupid, d.py-self supports cmds

--- bot.py
+++ bot.py
@@ -5,8 +5,6 @@ from dotenv import load_dotenv
 import os
 from flask import Flask
 from threading import Thread
-import requests
-import json
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -48,44 +46,9 @@ def keep_alive():
     t.start()
 
 async def send_bump_request(channel_id, guild_id):
-    headers = {
-        'Authorization': TOKEN,
-        'Content-Type': 'application/json',
-    }
-
-    payload = {
-        "type": 2,
-        "application_id": "302050872383242240",
-        "guild_id": str(guild_id),
-        "channel_id": str(channel_id),
-        "session_id": "d61f0ce1f0b8fd4570ae2a489fa75cd9",
-        "data": {
-            "version": "1051151064008769576",
-            "id": "947088344167366698",
-            "name": "bump",
-            "type": 1,
-            "options": [],
-            "application_command": {
-                "id": "947088344167366698",
-                "application_id": "302050872383242240",
-                "version": "1051151064008769576",
-                "default_member_permissions": None,
-                "type": 1,
-                "nsfw": False,
-                "name": "bump",
-                "description": "Pushes your server to the top of all your server's tags and the front page",
-                "description_localized": "Bump this server.",
-                "dm_permission": True,
-                "contexts": None
-            },
-            "attachments": []
-        },
-        "nonce": "1141916587096276992"
-    }
-
-    response = requests.post('https://canary.discord.com/api/v9/interactions', headers=headers, data=json.dumps(payload))
-
-    print(response.status_code)
+    guild = bot.get_guild(guild_id)
+    command = await guild.fetch_command('bump')
+    await command.invoke()
 
 keep_alive()
 bot.run(TOKEN)
