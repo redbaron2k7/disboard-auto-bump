0b925b7cc44575184d1ccd8e9fae1e29c6c33e28
redbaron2k7 <76609332+redbaron2k7@users.noreply.github.com>
2023-08-17 23:39:25 -0500

    Don't ask about before, it's fixed

--- bot.py
+++ bot.py
@@ -1,72 +1,51 @@
 import discord
-from discord.ext import commands
 import datetime
 from dotenv import load_dotenv
 import os
-import threading
-from flask import Flask
-import time
+import asyncio
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 PREFIX = os.getenv('BOT_PREFIX')
 CHANNEL_IDS = os.getenv('CHANNEL_IDS').split(',')
 
-bot = commands.Bot(command_prefix=PREFIX)
+client = discord.Client()
 
-@bot.listen()
+@client.event
 async def on_ready():
     print('Bot is ready.')
 
     while True:
-        threads = []
         for channel_id in CHANNEL_IDS:
-            channel = bot.get_channel(int(channel_id.strip()))
+            channel = client.get_channel(int(channel_id.strip()))
 
             if channel is not None:
                 guild_id = channel.guild.id
-                thread = threading.Thread(target=check_bump_availability, args=(channel.id, guild_id))
-                thread.start()
-                threads.append(thread)
+                async for message in channel.history():
+                    if message.author.id == 302050872383242240:  # This is the ID of Disboard
+                        embed = message.embeds[0]
+                        if 'Bump done' in embed.description:
+                            print("Bump detected")
+                            msg_time = message.created_at
+                            if (datetime.datetime.now(datetime.timezone.utc) - msg_time).total_seconds() > 7200:
+                                await channel.send('Bumping the server! Check my bio!')
+                                await send_bump_request(client, guild_id, channel)
+                        break
 
-        for thread in threads:
-            thread.join()
+        await asyncio.sleep(60)
 
-        time.sleep(5)
+async def send_bump_request(client, guild_id, channel):
+    guild = channel.guild
+    command_name = 'bump'
+    command = None
+    async for cmd in channel.slash_commands():
+        if cmd.name == command_name:
+            command = cmd
+            break
+    if command:
+        await command(channel=channel)
+    else:
+        print(f"Slash command '{command_name}' not found.")
 
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Hello. I am alive!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = threading.Thread(target=run)
-    t.start()
-
-async def send_bump_request(channel_id, guild_id):
-    guild = bot.get_guild(guild_id)
-    command = await guild.fetch_command('bump')
-    await command.invoke()
-
-def check_bump_availability(channel_id, guild_id):
-    channel = bot.get_channel(channel_id)
-    if channel is not None:
-        async def check_single_channel():
-            async for message in channel.history():
-                if message.author.id == 302050872383242240: # This is the ID of Disboard
-                    embed = message.embeds[0]
-                    if 'Bump done' in embed.description:
-                        print("Bump detected")
-                        msg_time = message.created_at
-                        if (datetime.datetime.now(datetime.timezone.utc) - msg_time).total_seconds() > 7200:
-                            await channel.send('Bumping Server...')
-                            await send_bump_request(channel.id, guild_id)
-                    break
-        asyncio.run(check_single_channel())
-
-keep_alive()
-bot.run(TOKEN)
+client.run(TOKEN)
